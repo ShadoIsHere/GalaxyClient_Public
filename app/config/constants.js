aa28640a98b431085b9c2ce9dd6d75d08e307030
ShadoIsHere <86681208+ShadoIsHere@users.noreply.github.com>
2021-12-20 18:49:40 +0100

    1.0.2

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,5 +1,5 @@
 const APP_DATA_NAME = 'Galaxy Client'
-const LIVE_DISTRIBUTION_URL = 'https://github.com/ShadoIsHere/GalaxyClient_Files/blob/main/distribution.json'
-const DEV_DISTRIBUTION_URL = 'https://github.com/ShadoIsHere/GalaxyClient_Files/blob/main/distribution.json'
+const LIVE_DISTRIBUTION_URL = 'https://launcher.modrealms.net/deploy/distribution.json'
+const DEV_DISTRIBUTION_URL = 'https://launcher.modrealms.net/dev-deploy/distribution.json'
 
 module.exports = { APP_DATA_NAME, LIVE_DISTRIBUTION_URL, DEV_DISTRIBUTION_URL }
